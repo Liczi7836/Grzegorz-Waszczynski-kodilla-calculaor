66f51512bc08d10079d3992cf3bb27f699a641b1
Grzegorz Waszczynski <grzegorzwaszczynski.real@gmail.com>
2021-11-01 11:48:09 +0100

    First Commit

--- src/main/java/com/kodilla/calculator/Calculator.java
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -1,4 +1,35 @@
 package com.kodilla.calculator;
 
+import java.util.Random;
+
 public class Calculator {
+
+    int x;
+    int y;
+    int result;
+
+        public Calculator(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public void addXandY(){
+
+           result = x + y;
+            System.out.println(result);
+        }
+
+        public void subtractXandY(){
+
+            result = x - y;
+            System.out.println(result);
+        }
+
+        public static void main(String args[]){
+
+            Calculator calculator = new Calculator(14,8);
+                    calculator.addXandY();
+                    calculator.subtractXandY();
+        }
+
 }
