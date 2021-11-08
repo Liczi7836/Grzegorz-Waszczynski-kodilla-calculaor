c91ba9ba20dadcbbcf7e6f2c5e8ec22063ad8d9f
Grzegorz Waszczynski <grzegorzwaszczynski.real@gmail.com>
2021-11-08 18:55:45 +0100

    Commit 18:52

--- src/main/java/com/kodilla/calculator/BetterCalculator.java
+++ src/main/java/com/kodilla/calculator/BetterCalculator.java
@@ -3,13 +3,26 @@ package com.kodilla.calculator;
 public class BetterCalculator {
 
     public int add(int a, int b, int c, int d){
-        return a+b;
+        if(a > c) {
+            return a + b;
+        } else if (c > a) {
+            return c + d;
+        }
+        else {
+            return c+b;
+        }
+        }
 
 
-    }
 
-    public int subtract(int a, int b){
-        return a-b;
+    public int subtract(int a, int b, int c){
+    int d = 0;
+       for(int i = 0; i < c; i++) {
+           d = d - b;
+
+
+       }
+        return a + d;
     }
 
 }
